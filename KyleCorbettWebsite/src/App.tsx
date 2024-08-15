907170a0f5d7feef13774580460015a5cc3764a2
Kyle Corbett <kyle19375corbett@gmail.com>
2024-08-15 07:22:16 +0200

    Adding of the Navbar

--- KyleCorbettWebsite/src/App.tsx
+++ KyleCorbettWebsite/src/App.tsx
@@ -8,7 +8,16 @@ function App() {
 
   return (
     <>
-     
+      <div className="navbar">
+        <a href="#home" className="active">Home</a>
+        <a href="#services">Services</a>
+        <a href="#about">About</a>
+        <a href="#contact">Contact</a>
+        <div className="navbar-right">
+            <a href="#login">Login</a>
+        </div>
+    </div>
+
 
 
     </>
